433a4bdec094571325796780c1b913ace47be5b0
Kyle <kblakley2121@gmail.com>
2022-10-24 21:55:05 -0700

    Set up React

---
+++ game-project/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Hamtaro from './HomeFiller/Hamtaro'
+const App = () => {
+    return (
+      <Hamtaro/>
+    )
+  }
+  
+  export default App
---
+++ game-project/src/HomeFiller/Hamtaro.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HamtaroPic from '../Assets/Hamtaro.png'
+
+function Hamtaro() {
+  return (
+    <div>
+        <img src={HamtaroPic} alt=""/>
+        <h1>Test</h1>
+    </div>
+  )
+}
+
+export default Hamtaro
